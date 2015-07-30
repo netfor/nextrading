99963c8ff2d6042fc406cdba610fb375ef2e334b
programador2 <programador2@172.16.1.161>
2015-07-29 20:56:13 -0500

    Parcial de nextrading

--- diseno/userFiles/js/main.js
+++ diseno/userFiles/js/main.js
@@ -5,16 +5,44 @@
 $(document).ready(function () {
 //Fullpager
     $('#fullpage').fullpage({
+        anchors: ["sec1", "sec2", "sec3", "sec4", "sec5", "sec6", "sec7"],
         animateAnchor: true,
         scrollOverflow: true,
         css3: false,
         slidesNavigation: true,
+        afterRender: function () {
+            animarHome();
+        },
+        onSlideLeave: function () {
+            animarHome();
+        }
     });
     //Aqui va el menu
     $("#main_menu_responsive").mmenu({
         "navbar": {
             title: "Nextrading"
         }
+    });
+    //Slider 
+    $(".sliderOn").bxSlider();
+
+    $(".sliderOnPrensa").bxSlider({
+        //pager: false
+    });
+    //Carrousel
+    $('.carrouselOnPrensa').bxSlider({
+        minSlides: 3,
+        maxSlides: 10,
+        slideWidth: 170,
+        slideMargin: 10,
+        pager: false,
+    });
+    //Carrousel
+    $('.carrouselVerInPrensa').bxSlider({
+        mode: 'vertical',
+        minSlides: 6,
+        maxSlides: 15,
+        slideMargin: 20
     });
     /** Animaciones usando Tween max **/
     function animarHome() {
@@ -25,13 +53,10 @@ $(document).ready(function () {
                 ])
                 .append([
                     TweenMax.fromTo($('.txt_home'), 1, {css: {marginLeft: "0%", opacity: 0}}, {css: {marginLeft: "5%", opacity: 1}}),
-                ])
-                .append([
-                    TweenMax.fromTo($('.albert'), 1, {css: {right: -100, opacity: 0}}, {css: {right: 0, opacity: 1}})
                 ]);
     }
 
     setInterval(function () {
-        $.fn.fullpage.moveSlideRight();
+        //$.fn.fullpage.moveSlideRight();
     }, 4000);
 });
